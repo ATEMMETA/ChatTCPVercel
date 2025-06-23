ce935243c7162cb0099417b21365020203df7407
ATEMMETA <atem.86796@gmail.com>
2025-06-22 20:31:55 -0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,33 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config')
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname)
-
-config.resolver.sourceExts.push('sql')
-config.resolver.assetExts.push('gguf')
-
-module.exports = config
-
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
+  return {
+    ...config,
+    transformer: {
+      ...config.transformer,
+      minifierPath: 'metro-minify-terser',
+      minifierConfig: {
+        keep_classnames: true,
+        keep_fnames: true,
+        mangle: false
+      }
+    },
+    server: {
+      ...config.server,
+      enhanceMiddleware: (middleware) => {
+        return (req, res, next) => {
+          console.log(`[Metro] Processing request: ${req.url}`);
+          return middleware(req, res, next);
+        };
+      }
+    },
+    resolver: {
+      ...config.resolver,
+      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs']
+    },
+    maxWorkers: 2,
+    resetCache: true
+  };
+})();
